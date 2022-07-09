cdc7c742d3d7e5d05752bb0da751f18dcf11ad1a
daved01 <david.kirchhoff@icloud.com>
2022-07-09 10:33:21 +0200

    Initial Commit

---
+++ LiveCameraSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LiveCameraSwiftUI
+//
+//  Created by David Kirchhoff on 2022-07-09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LiveCameraSwiftUI/LiveCameraSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  LiveCameraSwiftUIApp.swift
+//  LiveCameraSwiftUI
+//
+//  Created by David Kirchhoff on 2022-07-09.
+//
+
+import SwiftUI
+
+@main
+struct LiveCameraSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
